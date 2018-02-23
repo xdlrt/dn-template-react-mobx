0ba5b7731838b73499d8a773e746802545abe01b
yeshu.lrt <yeshu.lrt@alibaba-inc.com>
2018-02-23 14:27:21 +0800

    [modify]添加底部相关说明

--- src/components/todoApp.js
+++ src/components/todoApp.js
@@ -4,12 +4,22 @@ import TodoList from './todoList';
 
 export default function TodoApp() {
   return (
-    <section className="todoapp">
-      <header className="header">
-        <h1>todos</h1>
-        <TodoEntry />
-      </header>
-      <TodoList />
-    </section>
+    <div>
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <TodoEntry />
+        </header>
+        <TodoList />
+      </section>
+      <footer className="info">
+        <p>Double-click to edit a todo</p>
+        <p>
+          TodoMVC powered by React and <a href="http://github.com/mobxjs/mobx/">MobX</a>.
+          Created by <a href="http://github.com/xdlrt/">Icarus</a>
+        </p>
+        <p>Based on the base React TodoMVC by <a href="http://github.com/petehunt/">petehunt</a></p>
+      </footer>
+    </div>
   );
 }
